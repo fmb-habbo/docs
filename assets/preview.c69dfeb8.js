11ddf737e235a0cfce44feb1559042097d08ca56
fmb-habbo <yanlyra3@gmail.com>
2022-12-02 17:42:47 +0000

    Update from https://github.com/ylyra/fmb-habbo/commit/249aed47cf3461034f0e332985317d96b1cee70f

---
+++ assets/preview.c69dfeb8.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.3e8a3a46.js";import"./iframe.377ad41b.js";import"./es.map.constructor.1f538c83.js";import"./es.number.to-fixed.138610bd.js";import"./index.fb44a874.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.c69dfeb8.js.map
